fbf2b7f76e5b7d18eaa195522006af32ddd4fae2
MikhailValuyskiy <Mikhail.Skiy@gmail.com>
2024-01-24 14:03:56 +0300

    Updated tests
    Updated tests and dependencies

--- app/src/main/java/ru/androidschool/roomrelations/DbApp.kt
+++ app/src/main/java/ru/androidschool/roomrelations/DbApp.kt
@@ -1,27 +1,12 @@
 package ru.androidschool.roomrelations
 
 import android.app.Application
-import com.facebook.flipper.android.AndroidFlipperClient
-import com.facebook.flipper.android.utils.FlipperUtils
-import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
-import com.facebook.flipper.plugins.inspector.DescriptorMapping
-import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
-import com.facebook.soloader.SoLoader
 
 
 class DbApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        SoLoader.init(this, false)
-        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(
-                this
-            )
-        ) {
-            val client = AndroidFlipperClient.getInstance(this)
-            client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
-            client.addPlugin( DatabasesFlipperPlugin(this));
-            client.start()
-        }
+
     }
 }
